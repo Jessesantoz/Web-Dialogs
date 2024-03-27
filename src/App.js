5feb03c5378ace81dec844546d573fa34ff4ce61
Jessé Santoz <j-esse@hotmail.com.br>
2024-03-27 10:23:38 -0300

    Sistema de navegação entre as paginas sem troca de URL

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+
+//import NovoComentario from "./components/NovoComentario/NovoComentario"
+//import Comentario from "./components/Comentario/Comentario"
+import Apresentacao from "./components/Apresentacao/Apresentacao"
 
 
 function App() {
   return (
-
-    <div className='App'>
-      <header>
-        <nav>
-          <ul>
-            <li><a href='/'>Início</a></li>
-            <li><a href='/'>Usuario</a></li>
-            <li><a href='/'>Diálogos</a></li>
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/Apresentacao'>Início</Link>
+              </li>
+              <li>
+                <Link to='/'>Novo Diálogo</Link>
+              </li>
+              <li>
+                <Link to='/'>Diálogos</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/Apresentacao">
+              <Apresentacao/>
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
 
   )
 }
---
+++ src/components/apresentacao/apresentacao.js
@@ -0,0 +1,9 @@
+
+function Apresentacao() {
+    return <>
+        <h1> Pagina Inicial</h1>
+        <p>Bem vindo</p>
+        </>
+}
+
+export default Apresentacao
