a4ce3de06259dda009e4517feb82db3474c9b2f7
Jessé Santoz <j-esse@hotmail.com.br>
2024-04-02 09:02:30 -0300

    Conteúdo da Apresentação

--- src/components/apresentacao/apresentacao.js
+++ src/components/apresentacao/apresentacao.js
@@ -1,12 +1,36 @@
 
+import "./Apresentacao.css"
+
 function Apresentacao() {
     return <>
         <h1> Web Dialogs</h1>
-        <p>Frase de efeito</p>
-        <p>Explicação detalhada do site</p>
-        <p>Manual</p>
-        <p>Contatos</p>
-        <p>links de acesso</p>
+
+        <div className="App">
+            <div className="Ap">
+                <h3>Aqui você desenvolve conhecimento da maneira mais antiga, utilizando o que há de mais novo no mundo</h3>
+                <p>A Web Dialogs é uma inovadora ferramenta online que proporciona um espaço virtual para discussões e debates inspirados nos diálogos filosóficos da antiguidade. Com base nas metodologias socráticas de questionamento e reflexão, a plataforma promove um ambiente colaborativo e enriquecedor, onde os participantes podem compartilhar ideias, argumentos e experiências de forma respeitosa e construtiva.</p>
+                <p>Os usuários têm a oportunidade de explorar temas diversos, desde ética e política até metafísica e epistemologia, guiados por um moderador experiente que estimula a participação e o pensamento crítico. Além disso, a plataforma oferece recursos interativos, como salas de bate-papo e fóruns de discussão, que permitem uma maior interação entre os participantes e aprofundamento dos temas abordados.</p>
+                <p>Com a Plataforma Diálogos Antigos, buscamos resgatar a essência dos diálogos filosóficos da antiguidade, proporcionando um espaço virtual dinâmico e estimulante para a busca do conhecimento e da sabedoria.</p>
+            </div>
+
+            <div>
+                <h3>Manual:</h3>
+                <p>A Plataforma Diálogos Antigos oferece uma experiência simples e acessível aos seus usuários. Para começar, basta acessar o site pelo navegador web, onde você terá acesso a uma interface intuitiva e amigável. Lá, você poderá explorar os diálogos criados por outros usuários, mergulhando em discussões sobre diversos temas filosóficos.</p>
+                <p>Para participar ativamente, basta criar sua conta na plataforma, o que lhe dará acesso completo às funcionalidades. Com sua conta, você poderá criar seus próprios diálogos, tanto públicos quanto privados, além de poder participar de diálogos em grupo, com até 4 pessoas. Essa abordagem colaborativa e flexível permite que você desenvolva seus próprios pensamentos e contribua para uma comunidade de aprendizado mútuo e enriquecedor.</p>
+            </div>
+
+            <div>
+                <h3>Contatos:</h3>
+                <p>Instagram</p>
+                <p>Email</p>
+                <p>WhatsApp</p>
+            </div>
+
+            <div>
+                <h3>Links de Acesso:</h3>
+                <p>GitHub</p>
+            </div>
+        </div>
         </>
 }
 
